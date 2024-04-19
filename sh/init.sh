8491ad0c54479c31a6d9f400a848d0a14e9442e9
尤雨东 <root@ltpp.vip>
2024-04-20 07:45:41 +0800

    feat:update shell

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git remote add ltpp ssh://git@git.ltpp.vip:66/root/post-blog-user-crawler.git;
-git remote add github git@github.com:wmzn-ltpp/post-blog-user-crawler.git;
+git remote add ltpp ssh://git@git.ltpp.vip:66/root/OjJudgeTestdataCreat.git;
+git remote add github git@github.com:wmzn-ltpp/OjJudgeTestdataCreat.git;
