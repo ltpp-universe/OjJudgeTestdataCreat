01828a266cafe3a8fab82afdbdf5d9cf692791c5
尤雨东 <root@ltpp.vip>
2024-04-20 07:41:27 +0800

    feat:shell update

--- sh/push.sh
+++ sh/push.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 git add .;
-git commit -m "feat:update";
+echo -n "请输入更新消息并按回车键: "
+read update_message
+if [ -z "$update_message" ]; then
+    echo "更新消息不能为空，请重新运行脚本并输入有效的更新消息。"
+    echo "按回车键退出...";
+    read -n 1;
+    exit 1
+fi
+git commit -m "feat:$update_message";
 git push github main;
 echo -e "\e[32mgithub push finish\e[0m";
 git push ltpp main;
