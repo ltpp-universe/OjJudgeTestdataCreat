932c8d7ce81515f67b51241aed96ee71b46a2994
尤雨东 <root@ltpp.vip>
2024-06-04 21:16:33 +0800

    feat:update

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git remote add ltpp ssh://git@git.ltpp.vip:66/root/OjJudgeTestdataCreat.git;
-git remote add github git@github.com:wmzn-ltpp/OjJudgeTestdataCreat.git;
+git remote add github git@github.com:ltpp-universe/OjJudgeTestdataCreat.git;
